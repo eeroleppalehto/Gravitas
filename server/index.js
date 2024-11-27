d0c8194d2942bed9252eef491adfdf6e56931799
eeroleppalehto <eero.leppalehto@gmail.com>
2024-11-27 15:31:39 +0200

    unknown endpoint for server and serve css

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // Library imports
 const express = require('express');
 const path = require('path');
+const middleware = require('./utils/middleware')
 
 // Initialize express app
 const app = express();
@@ -10,7 +11,13 @@ const PORT = 3000;
 app.use(express.static('build'));
 app.get("/test", (_req, res) => {
     res.sendFile(path.join(__dirname + "/static/testaajille.html"));
-})
+});
+
+app.get("/static/tailwind.css", (_req, res) => {
+    res.sendFile(path.join(__dirname + "/static/tailwind.css"));
+});
+
+app.use(middleware.unknownEndpointBuilder(__dirname));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
