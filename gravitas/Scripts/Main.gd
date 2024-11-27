25c16de8f60a14bb6591416d24ed1c242d3a8dac
eeroleppalehto <eero.leppalehto@gmail.com>
2024-11-27 13:47:29 +0200

    Added button for different game mode

--- gravitas/Scripts/Main.gd
+++ gravitas/Scripts/Main.gd
@@ -8,4 +8,13 @@ func _on_quit_pressed():
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/world.tscn")
+	Gravity.mode = "scene"
+	get_tree().change_scene_to_file("res://scenes/world_scene_2/world2.tscn")
+
+
+func _on_play_b_pressed() -> void:
+	Gravity.mode = "player"
+	Gravity.isRotating = false
+	Gravity.currentGravityDirection = Gravity.SOUTH
+	Gravity.currentGravity = Gravity.GRAVITY_SOUTH
+	get_tree().change_scene_to_file("res://scenes/world_scene_2/world2.tscn")
