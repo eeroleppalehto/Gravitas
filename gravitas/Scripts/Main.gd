52ad27c90be2fe0dd086d1f66d04c74a9d134c23
eeroleppalehto <eero.leppalehto@gmail.com>
2024-11-27 15:30:46 +0200

    Reset all states when launching game

--- gravitas/Scripts/Main.gd
+++ gravitas/Scripts/Main.gd
@@ -8,13 +8,24 @@ func _on_quit_pressed():
 
 
 func _on_play_pressed():
+	# Set Gravity mode to "scene" and reset all states
 	Gravity.mode = "scene"
+	Gravity.isRotating = false
+	Gravity.currentGravityDirection = Gravity.SOUTH
+	Gravity.currentGravity = Gravity.GRAVITY_SOUTH
+	Gravity.isTurningRight = false
+	Gravity.isTurningLeft = false
+	Gravity.rotation_state = 0.0
 	get_tree().change_scene_to_file("res://scenes/world_scene_2/world2.tscn")
 
 
 func _on_play_b_pressed() -> void:
+	# Set Gravity mode to "player" and reset all states
 	Gravity.mode = "player"
 	Gravity.isRotating = false
 	Gravity.currentGravityDirection = Gravity.SOUTH
 	Gravity.currentGravity = Gravity.GRAVITY_SOUTH
+	Gravity.isTurningRight = false
+	Gravity.isTurningLeft = false
+	Gravity.rotation_state = 0.0
 	get_tree().change_scene_to_file("res://scenes/world_scene_2/world2.tscn")
